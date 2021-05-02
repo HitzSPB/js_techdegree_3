06e98aad24fdd50316db9cb031eaff610dbf54e8
HitzSPB <daniel.oesterbro@gmail.com>
2021-05-03 01:48:32 +0200

    Added very basic error validation but stays red forever

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Selected Elements that are reused
+const nameElement = document.querySelector("#name");
 const otherJobRoleField = document.querySelector("#other-job-role");
 const colorElement = document.querySelector("#color");
 const designElement = document.querySelector('#design');
@@ -11,6 +12,10 @@ const paymentMethods = document.querySelector("#payment-methods");
 const paypalElement = document.querySelector("#paypal");
 const bitcoinElement = document.querySelector("#bitcoin");
 const formElement = document.querySelector('form');
+const emailElement = document.querySelector('#email');
+const creditcardNumberElement = document.querySelector('#cc-num');
+const zipNumberElement = document.querySelector('#zip');
+const cvvNumberElement = document.querySelector('#cvv');
 
 // On page load
 otherJobRoleField.style.display = "none";
@@ -18,8 +23,16 @@ paypalElement.style.display = "none";
 bitcoinElement.style.display = "none";
 colorElement.disabled = true;
 paymentElement.selectedIndex = 1; // Selecting so default is credit card
-document.querySelector("#name").focus(); // Setting first textbox as focus
+nameElement.focus(); // Setting first textbox as focus
 // document.querySelector("#name").classList.add("error-text"); // Setting first textbox as focus
+
+// RegEx validators
+
+// Found regex for email validation :: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+const emailValidator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const cardnumberValidator = /^[0-9]{13,16}$/
+const zipnumberValidator = /^[0-9]{5}$/
+const cvvnumberValidator = /^[0-9]{3}$/
 
 // Functions
 
@@ -114,5 +127,35 @@ paymentElement.addEventListener('change', (event) => {
 
 formElement.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent reload page
-    console.log("test")
+    // Check name
+    if (nameElement.value === '') {
+        nameElement.classList.add('error');
+    }
+
+    // Check email
+    if (emailElement.value === '' || !emailValidator.test(emailElement.value)) {
+        emailElement.classList.add('error');
+    }
+
+    // Check activities selected
+    var anyChecked = false;
+    for (var count = 0; count <= activities.childElementCount - 1; count++) {
+        if (activities.children[count].children[0].checked) {
+            anyChecked = true;
+        }
+    }
+    if (!anyChecked) {
+        activities.classList.add('error');
+    }
+
+    // Credit card validation
+    if (paymentElement.value === "credit-card" && !cardnumberValidator.test(creditcardNumberElement.value)) {
+        creditcardNumberElement.classList.add("error")
+    }
+    if (paymentElement.value === "credit-card" && !zipnumberValidator.test(zipNumberElement.value)) {
+        zipNumberElement.classList.add("error")
+    }
+    if (paymentElement.value === "credit-card" && !cvvnumberValidator.test(cvvNumberElement.value)) {
+        cvvNumberElement.classList.add("error")
+    }
 });
